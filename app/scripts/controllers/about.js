d139c85471043cad93eca85639b91cd6e37c92a8
Andre <andre@localhost.localdomain>
2015-09-09 21:17:22 -0300

    commit initial. Yeoman angular completo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularComSassApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularComSassApp
+ */
+angular.module('angularComSassApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularComSassApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularComSassApp
+ */
+angular.module('angularComSassApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
