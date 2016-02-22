0fe47afd0c75b1f1da6fa75c2d71724f92820b38
André Pontes <andrepontes@gmail.com>
2016-02-22 17:16:18 -0300

    Removendo senhas do repositório

--- backend/src/main/java/ngimj/service/MySQLAccess.java
+++ backend/src/main/java/ngimj/service/MySQLAccess.java
@@ -16,7 +16,7 @@ public class MySQLAccess {
 
     private static final String URL = "jdbc:mysql://localhost:3306/imjoias";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "***REMOVED***";
+    private static final String PASSWORD = "";
 
     public Connection getConnection() {
         Connection connection = null;
