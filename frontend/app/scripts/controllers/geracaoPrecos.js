36f4bc75f1edeb4ef9d871a986df301333973f67
André Pontes <andrepontes@gmail.com>
2016-05-23 07:48:50 -0300

    protótipo para geração de preços.

--- frontend/app/scripts/controllers/geracaoPrecos.js
+++ frontend/app/scripts/controllers/geracaoPrecos.js
@@ -4,6 +4,10 @@ angular.module('app')
   .controller('GeracaoPrecosCtrl', ['$scope', '$routeParams', 'geracaoPrecosFactory',
     function($scope, $routeParams, geracaoPrecosFactory) {
 
+      geracaoPrecosFactory.get({}, function(geracaoPrecosFactory) {
+        $scope.geracaoPrecos  = geracaoPrecosFactory;
+      });
+
       return [];
     }
   ]);
--- frontend/app/scripts/services/geracaoPrecosFactory.js
+++ frontend/app/scripts/services/geracaoPrecosFactory.js
@@ -3,7 +3,7 @@
 angular.module('app')
   .factory('geracaoPrecosFactory', ['$resource', 'constantes',
     function($resource, constantes) {
-      return $resource(constantes.url + 'geracaoprecos', {}, {
+      return $resource(constantes.url + 'geracaotabela', {}, {
         get: {
           method: 'GET',
           cache: false,
