e138d1c3cfdb730090455c6e64e6d88fe91cfc82
André Pontes <andrepontes@gmail.com>
2016-05-30 09:21:28 -0300

    Formatação da referência - refatoração

--- frontend/app/scripts/filters/referenciaFormatada.js
+++ frontend/app/scripts/filters/referenciaFormatada.js
@@ -3,28 +3,29 @@
 angular.module('app')
   .filter('referenciaFormatada', function() {
 
+    var TAMANHO_MINIMO_REFERENCIA = 11;
+
+    var naoPodeFormatar = function(entrada) {
+      return entrada.length < TAMANHO_MINIMO_REFERENCIA
+    };
+
+    var possuiDigito = function(entrada) {
+      return entrada.length === 12;
+    };
+
+    var obterDigito = function(entrada) {
+      return entrada.substring(11, 12);
+    };
+
     return function(entrada) {
-
-      var TAMANHO_MINIMO_REFERENCIA = 11;
-
-      if (entrada.length < TAMANHO_MINIMO_REFERENCIA) {
+      if (naoPodeFormatar(entrada)) {
         return entrada;
       }
 
-      var entradaFormatada;
-
-      entradaFormatada = entrada.substring(0, 3) + '.' +
+      return entrada.substring(0, 3) + '.' +
         entrada.substring(3, 6) + '.' +
         entrada.substring(6, 8) + '.' +
-        entrada.substring(8, 11);
-
-      var entradaPossuiDigito = entrada.length === 12;
-
-      if (entradaPossuiDigito) {
-        var digito = entrada.substring(11, 12);
-        entradaFormatada += '-' + digito;
-      }
-
-      return entradaFormatada;
-    };
+        entrada.substring(8, 11) +
+        (possuiDigito(entrada) ? '-' + obterDigito(entrada) : '');
+    }
   });
