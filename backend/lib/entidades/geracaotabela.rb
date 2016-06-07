7bb1ec89466bea77f9fc5fcad8eb626e7750b2ea
André Pontes <andrepontes@gmail.com>
2016-06-07 10:22:35 -0300

    Rubocop.

--- backend/lib/entidades/geracaotabela.rb
+++ backend/lib/entidades/geracaotabela.rb
@@ -1,26 +1,27 @@
 require 'grape-entity'
 
 module ImjoiasGrape
-    module Entidades
-        class GeracaoTabela < Grape::Entity
-            format_with(:iso_timestamp) { |dt| dt.iso8601 }
+  module Entidades
+    # Entidade Geracao Tabela
+    class GeracaoTabela < Grape::Entity
+      format_with(:iso_timestamp, &:iso8601)
 
-            with_options(format_with: :iso_timestamp) do
-              expose :data
-            end
+      with_options(format_with: :iso_timestamp) do
+        expose :data
+      end
 
-            expose :juros
-            expose :ouro
+      expose :juros
+      expose :ouro
 
-            private
-              def ouro
-                "%f" % "#{object[:ouro]}"
-              end
+      private
 
-              def juros
-                object[:juros] ? "%f" % "#{object[:juros]}" : nil
-              end
+      def ouro
+        format('%f', object[:ouro].to_s)
+      end
 
-            end
+      def juros
+        object[:juros] ? format('%f', object[:juros]) : nil
+      end
     end
+  end
 end
--- backend/lib/negocio/geracaotabela.rb
+++ backend/lib/negocio/geracaotabela.rb
@@ -5,21 +5,21 @@ require 'grape-entity'
 require 'grape'
 
 module ImjoiasGrape
-    module Negocio
-        # GaracaoTabela
-        class GeracaoTabela < Entidade
-            def self.todas
-                bd[:geracaotabela].all
-            end
+  module Negocio
+    # GaracaoTabela
+    class GeracaoTabela < Entidade
+      def self.todas
+        bd[:geracaotabela].all
+      end
 
-            def self.obtem(data)
-                bd[:geracaotabela].where(data: data)
-            end
+      def self.obtem(data)
+        bd[:geracaotabela].where(data: data)
+      end
 
-            def self.gera(funcionario, ouro, juros)
-                args = [funcionario, ouro, juros]
-                bd.call_sproc('gerartabela', args: args)
-            end
-        end
+      def self.gera(funcionario, ouro, juros)
+        args = [funcionario, ouro, juros]
+        bd.call_sproc('gerartabela', args: args)
+      end
     end
+  end
 end
